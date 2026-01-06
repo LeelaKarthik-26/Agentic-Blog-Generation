101bb023989a85d3fb5b2d411cfbe792fee45b73
LeelaKarthik-26 <leelakarthik101@gmail.com>
2026-01-05 20:10:30 -0500

    init agentic blog

--- src/llms/groqllm.py
+++ src/llms/groqllm.py
@@ -7,9 +7,6 @@ class Groqllm:
         load_dotenv()
 
     def get_llm(self):
-        try:
-            os.environ["GROQ_API_KEY"] = self.groq_api_key = os.getenv("GROQ_API_KEY")
-            llm = init_chat_model(model="llama-3.1-8b-instant", model_provider="groq", api_key= self.groq_api_key)
-            return llm
-        except Exception as e:
-            raise ValueError(f"Error occured with exception: {e}")
+        os.environ["GROQ_API_KEY"] = self.groq_api_key = os.getenv("GROQ_API_KEY")
+        llm = init_chat_model(model="groq/llama-3.1-8b-instant", api_key= self.groq_api_key)
+        return llm
